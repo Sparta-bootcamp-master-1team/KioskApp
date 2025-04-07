910553cf8fb4ed6d6f6bbb8cedf0572e503617f9
minjae <qlqlzbto@naver.com>
2025-04-08 01:38:46 +0900

    feat: DataService의 fetchData 함수생성

--- KioskApp/KioskApp/DataService.swift
+++ KioskApp/KioskApp/DataService.swift
@@ -1,5 +1,25 @@
 import Foundation
 
+enum DataError: Error {
+    case fileNotFound
+    case parsingFailed
+}
+
 class DataService {
-    
+    var items: Product?
+    func fetchData(completion: @escaping (Result< Product, Error>) -> Void) {
+        guard let path = Bundle.main.path(forResource: "Beverages", ofType: "json") else {
+            completion(.failure(DataError.fileNotFound))
+            return
+        }
+        
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            let bookResponse = try JSONDecoder().decode(Product.self, from: data)
+            completion(.success(bookResponse))
+        } catch {
+            print("JSON 파싱 에러 : \(error)")
+            completion(.failure(DataError.parsingFailed))
+        }
+    }
 }
