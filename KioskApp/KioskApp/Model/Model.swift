1c7f3a03a2311fa478869920be15c6c69e510195
minjae <qlqlzbto@naver.com>
2025-04-08 01:39:02 +0900

    feat: 모델 구성

--- KioskApp/KioskApp/Model/Model.swift
+++ KioskApp/KioskApp/Model/Model.swift
@@ -1,1 +1,17 @@
 import Foundation
+
+struct Product: Decodable {
+    let bevarage: [Beverage]
+}
+
+struct Beverage: Decodable {
+    let name: String
+    let price: Int
+    let category: String
+    let option: String?
+    let brand: String
+    
+    var imageString: String {
+        return "\(self.category)" + "\(self.option)" + "\(self.name)"
+    }
+}
