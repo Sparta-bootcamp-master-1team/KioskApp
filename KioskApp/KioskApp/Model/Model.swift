23e15f0d3e59383e2bd2b6847c84c4cec1c547f8
minjae <qlqlzbto@naver.com>
2025-04-08 14:53:11 +0900

    refactor: 카테고리, 브랜드, 옵션을 열거형으로 구현하고 해당 설정에 맞는 데이터를 접근할 메서드 구현

--- KioskApp/KioskApp/Model/Model.swift
+++ KioskApp/KioskApp/Model/Model.swift
@@ -1,15 +1,47 @@
 import Foundation
 
+enum Category: String, Decodable {
+    case Coffee
+    case Beverage
+    case Desert
+}
+
+enum Brand: String, Decodable {
+    case Baik
+    case TheVenti
+    case Mega
+}
+
+enum Option: String, Decodable{
+    case HOT
+    case ICE
+}
+
 struct Product: Decodable {
     let beverage: [Beverage]
+    
+    var baiksProduct: [Beverage] {
+        return beverage.filter { $0.brand == .Baik }
+    }
+    
+    var theVentiProduct: [Beverage] {
+        return beverage.filter { $0.brand == .TheVenti }
+    }
+    var megaProduct: [Beverage] {
+        return beverage.filter { $0.brand == .Mega }
+    }
+    
+    func fetchData(brand: Brand, category: Category, option: Option? = nil) -> [Beverage] {
+        return beverage.filter { $0.category == category && $0.brand == brand && $0.option == option}
+    }
 }
 
 struct Beverage: Decodable {
     let name: String
     let price: Int
-    let category: String
-    let option: String?
-    let brand: String
+    let category: Category
+    let option: Option?
+    let brand: Brand
     
     var imageName: String {
         return "\(category)" + "\(option == nil ? "-" : "\(option!)-")" + "\(name)" + "-\(brand)"
