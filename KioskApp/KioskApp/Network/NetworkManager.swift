3cfdaa0c0c73920370e19423a08e120d4b817539
minjae <qlqlzbto@naver.com>
2025-04-09 14:28:30 +0900

    feat: NetworkManager의 fetchData 메서드 구현

--- KioskApp/KioskApp/Network/NetworkManager.swift
+++ KioskApp/KioskApp/Network/NetworkManager.swift
@@ -1,5 +1,48 @@
 import Foundation
 
-class NetworkManager {
+enum NetworkError: Error {
+    case decodingError
+    case serverError(code: Int)
+    case missingData
+    case invalidURL
+    case transportError
+}
+
+final class NetworkManager {
     
+    private let session = URLSession(configuration: .default)
+    private let urlComponenetHandler = URLComponentHandler()
+    
+    func fetchData<T: Decodable>(for type: T.Type ,
+                                 brand: Brand? = nil,
+                                 category: Category? = nil,
+                                 option: Option? = nil) async throws -> T {
+        let urlComponents = urlComponenetHandler.fetchURLComponents(brand: brand, category: category, option: option)
+        
+        guard let url = urlComponents.url else {
+            throw(NetworkError.invalidURL)
+        }
+        
+        var urlRequest = URLRequest(url: urlComponents.url!)
+        urlRequest.httpMethod = "GET"
+        
+        let response = try await session.data(for: urlRequest)
+        
+        guard let httpResponse = response.1 as? HTTPURLResponse else {
+            throw(NetworkError.transportError)
+        }
+        let successRange = 200..<300
+        guard successRange.contains(httpResponse.statusCode) else {
+            let code = httpResponse.statusCode
+            throw(NetworkError.serverError(code: code))
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let res: T = try decoder.decode(T.self, from: response.0)
+            return res
+        } catch {
+            throw(NetworkError.decodingError)
+        }
+    }
 }
